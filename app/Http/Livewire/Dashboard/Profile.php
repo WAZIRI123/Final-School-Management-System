5908ac72bf24cd8ac3f67cd29d93c1b473866018
waziri123 <waziriallyamiri123@gmail.com>
2022-11-08 06:10:54 -0800

    update profile test

--- app/Http/Livewire/Dashboard/Profile.php
+++ app/Http/Livewire/Dashboard/Profile.php
@@ -14,22 +14,34 @@ class Profile extends Component
     public $password;
     public $password_confirmation;
     public $user;
+
+    protected function rules()
+    {
+    return[
+        'name' => ['required', 'string', 'max:255'],
+        'email' => ['required', 'string', 'email', Rule::unique('users','email')->ignore($this->user->id)->whereNull('deleted_at')],
+        'password' => ['confirmed',Password::defaults()],
+    ];
+}
     public function mount(){
         $this->user=User::where('id',auth()->user()->id)->first();
         $this->name=$this->user->name;
         $this->email=$this->user->email;
     }
     public function updateUser(){
-        $validatedData = $this->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', Rule::unique('users','email')->ignore($this->user->id)->whereNull('deleted_at')],
-            'password' => ['confirmed',Password::defaults()],
-        ]);
 
- 
-        auth()->user()->update($validatedData);
+        $this->validate();
+        $this->user->update([
+            'name'=>$this->name,
+            'email'=>$this->email,
+        ]
+        );
         if ($this->password) {
-            auth()->user()->update(['password'=>bcrypt($validatedData['password'])]);
+            $this->user->update([
+                'password'=>bcrypt($this->password),
+                'name'=>$this->name,
+                'email'=>$this->email,
+            ]);
          
         }
         $this->dispatchBrowserEvent('name-updated');
---
+++ tests/Feature/Livewire/User/ProfileTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature\Livewire\User;
+
+use App\Http\Livewire\Dashboard\Profile;
+use App\Models\User;
+use App\Traits\FeatureTestTrait;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class ProfileTest extends TestCase
+{
+    use RefreshDatabase;
+    use FeatureTestTrait, AuthorizesRequests;
+
+        //test authorised users can edit parents
+        public function test_authorised_users_can_edit_user()
+        {
+    
+    
+            // make fake user && assign role && acting as that user
+            $user1 = User::factory()->create();
+    
+            // test
+            Livewire::actingAs($user1)
+                ->test(Profile::class)
+
+                ->set('name', 'waziribig')
+                ->set('email', 'waziri567@gmail.com')
+                ->set('password', '0653039317')
+                ->set('password_confirmation', '0653039317')
+                ->call('updateUser');
+    
+            // test if data exist in database
+            $this->assertDatabaseHas('users', [
+                'name' => 'waziribig'
+            ]);
+
+        }
+
+}
