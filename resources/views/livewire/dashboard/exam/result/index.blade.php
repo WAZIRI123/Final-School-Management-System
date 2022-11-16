712016989c6f7bb18ac1052cd53acd2b810f21a3
waziri123 <waziriallyamiri123@gmail.com>
2022-11-16 05:25:33 -0800

    student result continue

--- resources/views/livewire/dashboard/exam/result/index.blade.php
+++ resources/views/livewire/dashboard/exam/result/index.blade.php
@@ -34,7 +34,7 @@
                             <tr class="bg-secondary text-gray-100 font-bold">
                                 <td class="px-3 py-2 capitalize">Subjects</td>
                                 <td class="px-3 py-2 capitalize">Marks</td>
-                             
+                                <td class="px-3 py-2 capitalize">Remark</td>
                             </tr>
                         </thead>
                         <tbody class="divide-y divide-blue-400">
@@ -42,6 +42,34 @@
                             <tr class="hover:bg-blue-300 {{ ($loop->even ) ? " bg-blue-100" : "" }}">
                                 <td class="px-3 py-2 capitalize">{{ $result->subjects?->name }}</td>
                                 <td class="px-3 py-2 capitalize">{{ $result->marks }}</td>
+                                @switch($result->marks)
+                                @case($result->marks>=80)
+                                <td class="px-3 py-2 capitalize">Excellence</td>
+                                    @break
+
+                                    @case($result->marks>=70 && $result->marks<80 )
+                                    <td class="px-3 py-2 capitalize">Very Good</td>
+                                    @break
+
+                                    @case($result->marks>=60 && $result->marks<70 )
+                                    <td class="px-3 py-2 capitalize">Good</td>
+                                    @break
+                            
+                                    @case($result->marks>=50 && $result->marks<60 )
+                                    <td class="px-3 py-2 capitalize">Credit</td>
+                                    @break
+
+                                    @case($result->marks>=30 && $result->marks<50 )
+                                    <td class="px-3 py-2 capitalize">Average</td>
+                                    @break
+
+                                    @case($result->marks<30 )
+                                    <td class="px-3 py-2 capitalize">Fail</td>
+                                    @break
+
+                                @default
+                                    
+                            @endswitch
                             </tr>
                             @endforeach
 
@@ -53,7 +81,7 @@
                             <tr class="bg-secondary text-gray-100 font-bold">
                                 <td class="px-3 py-2 capitalize">Subjects</td>
                                 <td class="px-3 py-2 capitalize">Marks</td>
-                             
+                                <td class="px-3 py-2 capitalize">Remark</td>
                             </tr>
                         </thead>
                         <tbody class="divide-y divide-blue-400">
@@ -61,6 +89,34 @@
                             <tr class="hover:bg-blue-300 {{ ($loop->even ) ? " bg-blue-100" : "" }}">
                                 <td class="px-3 py-2 capitalize">{{ $result->subjects?->name }}</td>
                                 <td class="px-3 py-2 capitalize">{{ $result->marks }}</td>
+                                @switch($result->marks)
+                                    @case($result->marks>=80)
+                                    <td class="px-3 py-2 capitalize">Excellence</td>
+                                        @break
+
+                                        @case($result->marks>=70 && $result->marks<80 )
+                                        <td class="px-3 py-2 capitalize">Very Good</td>
+                                        @break
+
+                                        @case($result->marks>=60 && $result->marks<70 )
+                                        <td class="px-3 py-2 capitalize">Good</td>
+                                        @break
+                                
+                                        @case($result->marks>=50 && $result->marks<60 )
+                                        <td class="px-3 py-2 capitalize">Credit</td>
+                                        @break
+
+                                        @case($result->marks>=30 && $result->marks<50 )
+                                        <td class="px-3 py-2 capitalize">Average</td>
+                                        @break
+
+                                        @case($result->marks<30 )
+                                        <td class="px-3 py-2 capitalize">Fail</td>
+                                        @break
+
+                                    @default
+                                        
+                                @endswitch
                             </tr>
                             @endforeach
 
